c63cf24fa5d7ea34c4ab7b5122579b3f378fe24b
Gabriel <142633367+gabrielvilela12@users.noreply.github.com>
2026-01-11 19:46:19 -0300

    Delete 1 - Projetos 2026/Sistema Bancário Simples directory

--- 1 - Projetos 2026/Sistema Bancário Simples/Sistema Bancário Simples/0 - Presentation/ViewModel/UserTransferenceViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Sistema_Bancário_Simples.Presentation
-{
-    public class UserTransferenceViewModel
-    {
-        public int Id { get; internal set; }
-        public string Name { get; internal set; }
-        public string Email { get; internal set; }
-    }
-}
